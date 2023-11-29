a4b45f5c1efd609d23f877e627b4928f24c658d5
Tverskoy31 <medvedev-qa@mail.ru>
2023-11-29 14:49:46 +0300

    Revert "Add test for "ger_text""
    This reverts commit 8cdfeaae8f079d57a82bd670875992f299ca2b7a.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,9 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
+
     @Test
     public void testHelloWorld() {
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
+        System.out.println("Hello world");
     }
 }
