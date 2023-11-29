48d591804cb33c5553832240791d37970e8a66ed
Tverskoy31 <medvedev-qa@mail.ru>
2023-11-29 14:55:16 +0300

    Add test for "get_text"

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
@@ -5,5 +7,9 @@ public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
         System.out.println("Hello world");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
