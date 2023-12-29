5eaada811bab33a30ba07bb6d8a9f3581f257f7e
Tverskoy31 <medvedev-qa@mail.ru>
2023-12-30 01:42:32 +0300

    Добавил тест на длинный редирект

---
+++ src/test/java/LongRedirectTest.java
@@ -0,0 +1,23 @@
+package homework;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class LongRedirectTest {
+
+    //Задание №7
+    @Test
+    public  void redirectTest(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        int status = response.statusCode();
+        System.out.println(status);
+    }
+}
