e8156e7262f1e6504d359fb388ea04d2eed096f2
Tverskoy31 <medvedev-qa@mail.ru>
2023-12-30 12:59:07 +0300

    вернул пробел

--- src/test/java/LongRedirectTest.java
+++ src/test/java/LongRedirectTest.java
@@ -1,6 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
 public class LongRedirectTest {
     @Test
     public  void redirectTest(){
