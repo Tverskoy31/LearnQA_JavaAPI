8f303cc13556c87a4135f762d29bde24c6af102e
Tverskoy31 <medvedev-qa@mail.ru>
2024-01-02 23:16:32 +0300

    Add test "Ex8: Токены"

--- src/test/java/TokenTest.java
+++ src/test/java/TokenTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TokenTest {
